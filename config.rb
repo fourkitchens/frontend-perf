28de7dca427d997aaa858816536d32f9902c9103
Chris Ruppel <chris.ruppel@gmail.com>
2015-04-11 16:39:34 +0200

    create Sass stuff

---
+++ config.rb
@@ -0,0 +1,22 @@
+
+# Require any additional compass plugins above this line.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "_sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = false
+
+# Disable cache busting on image assets
+asset_cache_buster :none
