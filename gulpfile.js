82cf68fbb2dace693429f5b1bd28f25adee4de6e
Chris Ruppel <chris.ruppel@gmail.com>
2015-04-12 23:45:20 +0200

    error handling for CSS gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,11 @@ var sass = require('gulp-sass');
 gulp.task('css', function() {
   return gulp.src('_sass/**/*.scss')
     .pipe(sass({
-      outputStyle: 'nested'
+      outputStyle: 'nested',
+      onError: function(err, res) {
+        log(c.red('Sass failed to compile'));
+        log(c.red('> ') + err.file.split('/')[err.file.split('/').length - 1] + '@' + err.line + ': ' + err.message);
+      }
     }))
     .pipe(prefix("last 2 versions", "> 1%"))
     .pipe(gulp.dest('css'));
