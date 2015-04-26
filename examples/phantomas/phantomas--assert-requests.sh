9d6a5ca27aaf8aa0c67df4a9c7dc4ad13f711905
Chris Ruppel <chris.ruppel@gmail.com>
2015-04-26 20:27:44 +0200

    :bar_chart: phantomas bash test

---
+++ examples/phantomas/phantomas--assert-requests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+LIMIT=5
+
+echo "Phantomas: limit HTTP requests to $LIMIT"
+phantomas --url http://localhost:4000 --assert-requests=$LIMIT >/dev/null
+
+if [ $? -eq 0 ]
+then
+  echo "Phantomas: ✔︎ Yay! The site makes $LIMIT or fewer HTTP requests."
+  exit 0
+else
+  echo "Phantomas: ✘ Rats! The site makes more than $LIMIT HTTP requests." >&2
+  exit 1
+fi
